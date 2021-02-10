777263aa974034b6b7809d64eb26fa257f1ac187
Abdullah Al Noman <noman19bd@yahoo.com>
2021-02-10 12:27:37 +0600

    bKash Payment API with Refund API for Laravel

---
+++ Laravel/routes.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::group(['middleware' => ['customAuth']], function () {
+
+    // Payment Routes for bKash
+    Route::post('bkash/get-token', 'BkashController@getToken')->name('bkash-get-token');
+    Route::post('bkash/create-payment', 'BkashController@createPayment')->name('bkash-create-payment');
+    Route::post('bkash/execute-payment', 'BkashController@executePayment')->name('bkash-execute-payment');
+    Route::get('bkash/query-payment', 'BkashController@queryPayment')->name('bkash-query-payment');
+    Route::post('bkash/success', 'BkashController@bkashSuccess')->name('bkash-success');
+
+    // Refund Routes for bKash
+    Route::get('bkash/refund', 'BkashRefundController@index')->name('bkash-refund');
+    Route::post('bkash/refund', 'BkashRefundController@refund')->name('bkash-refund');
+
+});
